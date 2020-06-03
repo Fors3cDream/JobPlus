30b941b941013a239af2494e0d5fda78b8102c2d
Fors3cDream <xionx20@sina.cn>
2020-06-03 11:25:39 +0800

    Add base.html & index.html

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -5,3 +5,19 @@ front = Blueprint('front', __name__, url_prefix='/')
 @front.route('/')
 def index():
 	return render_template('index.html')
+
+@front.route('/login')
+def login():
+	pass
+
+@front.route('/logout')
+def logout():
+	pass
+
+@front.route('/userregister')
+def userregister():
+	pass
+
+@front.route('/companyregister')
+def companyregister():
+	pass
